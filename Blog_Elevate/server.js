9dfc8ddc9f4d96dbd5435934d989978d881a723d
Aurel-Rio <riozacki@gmx.com>
2023-05-26 16:53:07 +0200

    v.2

--- Blog_Elevate/server.js
+++ Blog_Elevate/server.js
@@ -1,26 +1,23 @@
 const express = require('express');
+const path = require('path');
 const app = express();
-const PORT = process.env.PORT || 5000; // Port d'écoute du serveur
+const PORT = process.env.PORT || 5000;
 
+// Middleware pour les fichiers statiques
+app.use(express.static(path.join(__dirname, 'blog_elevate/build')));
 
+// Vos routes d'API
 
-
-app.use(express.json());
-// Routes de votre API
-
-// Endpoint racine
+// Route pour la page d'accueil
 app.get('/', (req, res) => {
-  res.send('API Blog-Elevate');
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
 
+// Route pour les autres requêtes
+app.get('*', (req, res) => {
+  res.redirect('/');
+});
 
-const articlesRouter = require('./routes/articles');
-
-// Utiliser la route /articles
-app.use('/articles', articlesRouter);
-
-// Démarrer le serveur
 app.listen(PORT, () => {
   console.log(`Serveur en cours d'exécution sur le port ${PORT}`);
 });
-
